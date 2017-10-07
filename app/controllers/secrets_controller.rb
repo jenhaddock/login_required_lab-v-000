ec3f6d233dbc3279152439a34a0274eccbc3b5e1
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 23:18:07 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -6,7 +6,7 @@ class SecretsController < ApplicationController
     end
 
     def require_login
-      return head(:forbidden) unless session.include? :user_id    
+      return head(:forbidden) unless session.include? :user_id
     end
 
 end
