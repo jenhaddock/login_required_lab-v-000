e2d511f3051dc1951ebfcadfc92aabe6f0dcd2ee
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 23:51:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    if !params[:name].nil? && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
