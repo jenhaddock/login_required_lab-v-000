9bf82bba2801d682fe007b69c9e3f7a7d98a92ff
Jennifer Haddock <jentx123@hotmail.com>
2017-10-07 23:54:16 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,11 +2,11 @@ class SecretsController < ApplicationController
     before_action :require_login
 
     def show
-      render :show
+
     end
 
     def require_login
-      redirect_to '/login' unless session.include? :user_id
+      redirect_to '/login' unless session.include? :name
       #return head(:forbidden) unless session.include? :user_id
     end
 
